c8df9d5960875c7c501f3fc482bc387c6d10d932
Ryan De La Torre <rdelatorre@servicetitan.com>
2021-12-01 09:45:13 -0800

    debugging argv

--- dist/index.js
+++ dist/index.js
@@ -73,6 +73,8 @@ const exec = util.promisify((__nccwpck_require__(81).exec));
 const workspace = process.env["GITHUB_WORKSPACE"];
 const { packages } = JSON.parse(process.argv[2]);
 
+console.log("process.argv[2]:", process.argv[2]);
+console.log("JSON.parse(process.argv[2]):", JSON.parse(process.argv[2]));
 console.log("packages:", typeof packages, packages);
 
 updateAllDependencies();
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const exec = util.promisify(require("child_process").exec);
 const workspace = process.env["GITHUB_WORKSPACE"];
 const { packages } = JSON.parse(process.argv[2]);
 
+console.log("process.argv[2]:", process.argv[2]);
+console.log("JSON.parse(process.argv[2]):", JSON.parse(process.argv[2]));
 console.log("packages:", typeof packages, packages);
 
 updateAllDependencies();
