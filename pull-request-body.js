26d8d7b37618c418757713d66ec4e47a1f40119b
Ryan De La Torre <rdelatorre@servicetitan.com>
2022-03-17 00:22:12 -0700

    testing release notes

--- pull-request-body.js
+++ pull-request-body.js
@@ -4,51 +4,115 @@ const { Octokit } = require("@octokit/core");
 const token = process.argv[2];
 const matrix = JSON.parse(JSON.parse(process.argv[3]));
 
-// const octokit = new Octokit({ auth: token });
+// const token = "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";
+// const matrix = {
+//   packages: [
+//     {
+//       name: "jest",
+//       unscopedPackageName: "jest",
+//       currentVersion: "27.2.0",
+//       latestVersion: "27.5.1",
+//       url: "https://github.com/facebook/jest",
+//     },
+//     {
+//       name: "@types/jest",
+//       unscopedPackageName: "jest",
+//       currentVersion: "27.0.1",
+//       latestVersion: "27.4.1",
+//       url: "https://github.com/DefinitelyTyped/DefinitelyTyped",
+//     },
+//   ],
+//   scope: "",
+//   groupCurrentVersion: "27.2.0",
+//   groupLatestVersion: "27.5.1",
+//   semverLabel: "minor",
+//   displayName: "jest",
+//   hash: "ZYYpsF",
+// };
 
-let body = "";
+const octokit = new Octokit({ auth: token });
 
-body += listOfPackages(matrix.packages);
+(async function () {
+  let body = "";
 
-body += `<details>
+  body += listOfPackages(matrix.packages);
+
+  const versionsKeyedByURL = getVersionsKeyedByURL(matrix.packages);
+
+  for (const [url, version] of Object.entries(versionsKeyedByURL)) {
+    const owner = url.split("/").at(-2);
+    const repo = url.split("/").at(-1);
+
+    try {
+      const { body: releaseBody } = await getReleaseByVersion(
+        owner,
+        repo,
+        version
+      );
+
+      if (releaseBody) {
+        body += `<details>
 <summary>Release notes</summary>
-<p><em>Sourced from <a href="https://github.com/servicetitan/anvil/releases"><code>@​servicetitan/tokens</code>'s releases</a>.</em></p>
+<p><em>Sourced from <a href="${url}"><code>${owner}/${repo}</code>'s releases</a>.</em></p>
 <blockquote>
 
-## Features
-
-- \`[jest-config]\` Support comments in JSON config file ([#12316](https://github.com/facebook/jest/pull/12316))
-- \`[pretty-format]\` Expose \`ConvertAnsi\` plugin ([#12308](https://github.com/facebook/jest/pull/12308))
-
-## Fixes
-
-- \`[expect]\` Add type definitions for asymmetric \`closeTo\` matcher ([#12304](https://github.com/facebook/jest/pull/12304))
-- \`[jest-cli]\` Load binary via exported API ([#12315](https://github.com/facebook/jest/pull/12315))
-- \`[jest-config]\` Replace \`jsonlint\` with \`parse-json\` ([#12316](https://github.com/facebook/jest/pull/12316))
-- \`[jest-repl]\` Make module importable ([#12311](https://github.com/facebook/jest/pull/12311) & [#12315](https://github.com/facebook/jest/pull/12315))
-
-## Chore & Maintenance
-
-- \`[*]\` Avoid anonymous default exports ([#12313](https://github.com/facebook/jest/pull/12313))
-
-## New Contributors
-* @zoltan-boros made their first contribution in https://github.com/facebook/jest/pull/12206
-
-**Full Changelog**: https://github.com/facebook/jest/compare/v27.5.0...v27.5.1
+`;
+        body += releaseBody;
+        body += `
 
 </blockquote>
 </details>`;
+      }
+    } catch (e) {
+      console.error(e?.response);
+    }
+  }
 
-core.setOutput("body", body);
+  core.setOutput("body", body);
+})();
 
-function allUniqueURLs(packages) {
+function getVersionsKeyedByURL(packages) {
   const packagesWithoutTypes = packages.filter(
     ({ name }) => !name.startsWith("@types")
   );
 
-  const setOfURLs = new Set([...packagesWithoutTypes.map(({ url }) => url)]);
+  return packagesWithoutTypes.reduce((acc, { latestVersion, url }) => {
+    return { ...acc, [url]: latestVersion };
+  }, {});
+}
 
-  return [...setOfURLs];
+async function getReleaseByVersion(owner, repo, version) {
+  const possibleTags = [
+    `v${version}`,
+    version,
+    `${repo}@${version}`,
+    `${repo}@v${version}`,
+  ];
+
+  for (const tag of possibleTags) {
+    const response = await getReleaseByTag(owner, repo, tag);
+
+    if (response?.status === 200) {
+      return response?.data;
+    }
+  }
+}
+
+async function getReleaseByTag(owner, repo, tag) {
+  try {
+    const response = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases/tags/{tag}",
+      {
+        owner,
+        repo,
+        tag,
+      }
+    );
+
+    return response;
+  } catch (e) {
+    return e?.response;
+  }
 }
 
 function listOfPackages(packages) {
